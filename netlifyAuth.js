dfddad6bd6ed5be19308f0c303b7bc8280867878
duncanlaidlaw <slamdunc123@gmail.com>
2022-03-03 13:35:03 +0000

    fix click event not working after log in

--- netlifyAuth.js
+++ netlifyAuth.js
@@ -1,32 +1,33 @@
-import netlifyIdentity from 'netlify-identity-widget'
+import netlifyIdentity from 'netlify-identity-widget';
 
 const netlifyAuth = {
-  isAuthenticated: false,
-  user: null,
-  initialize(callback) {
-    window.netlifyIdentity = netlifyIdentity
-    netlifyIdentity.on('init', (user) => {
-      callback(user)
-    })
-    netlifyIdentity.init()
-  },
-  authenticate(callback) {
-    this.isAuthenticated = true
-    netlifyIdentity.open()
-    netlifyIdentity.on('login', (user) => {
-      this.user = user
-      callback(user)
-      netlifyIdentity.close()
-    })
-  },
-  signout(callback) {
-    this.isAuthenticated = false
-    netlifyIdentity.logout()
-    netlifyIdentity.on('logout', () => {
-      this.user = null
-      callback()
-    })
-  },
-}
+	isAuthenticated: false,
+	user: null,
+	initialize(callback) {
+		window.netlifyIdentity = netlifyIdentity;
+		netlifyIdentity.on('init', (user) => {
+			callback(user);
+		});
+		netlifyIdentity.init();
+	},
+	authenticate(callback) {
+		this.isAuthenticated = true;
+		netlifyIdentity.open();
+		netlifyIdentity.on('login', (user) => {
+			netlifyIdentity.close();
+			this.user = user;
+			callback(user);
+		});
+	},
+	signout(callback) {
+		this.isAuthenticated = false;
+		netlifyIdentity.logout();
+		netlifyIdentity.on('logout', () => {
+            netlifyIdentity.close()
+			this.user = null;
+			callback();
+		});
+	},
+};
 
-export default netlifyAuth
+export default netlifyAuth;
