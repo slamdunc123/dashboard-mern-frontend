4d98b022632fc27e33decf805a6f36902e14bb0c
duncanlaidlaw <slamdunc123@gmail.com>
2022-03-15 20:58:48 +0000

    rename Sidebar reference to PersistentDrawerLeft

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Sidebar from '../Sidebar/Sidebar';
+import PersistentDrawerLeft from '../Sidebar/Sidebar';
 
 type LayoutProps = {
 	children?: React.ReactNode;
@@ -9,7 +9,7 @@ type LayoutProps = {
 export default function Layout({ children }: LayoutProps) {
 	return (
 		<>
-			<Sidebar>{children}</Sidebar>
+			<PersistentDrawerLeft>{children}</PersistentDrawerLeft>
 		</>
 	);
 }
