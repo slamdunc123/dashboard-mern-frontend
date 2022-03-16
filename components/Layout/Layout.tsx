b82299c199bb65060e95f396d2c5c9979b9f2545
duncanlaidlaw <slamdunc123@gmail.com>
2022-03-16 07:50:05 +0000

    remove Layout page, Layout is a component not a page

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,10 +1,11 @@
+import Sidebar from "../Sidebar/Sidebar";
 
 type LayoutProps = {
 	children?: React.ReactNode;
 };
 
 const Layout = ({ children }: LayoutProps) => {
-	return <>{children}</>;
+	return <Sidebar>{children}</Sidebar>;
 };
 
 export default Layout;
--- pages/Layout.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Sidebar from '../Sidebar/Sidebar';
-
-const Layout = ({ children }: any) => {
-	return <Sidebar>{children}</Sidebar>;
-};
-
-export default Layout;
